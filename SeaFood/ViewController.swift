8c25a88809acbc8b3c7e27ff42d675d0e532ab46
Tasnim Haider Chaudhury <tasnimhaiderchaudhury@gmail.com>
2018-08-02 14:52:21 +0600

    Initial commit.

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -1,25 +1,28 @@
-//
-//  ViewController.swift
-//  SeaFood
-//
-//  Created by Tasnim Haider Chaudhury on 31/7/18.
-//  Copyright © 2018 Bita Consultancy Services. All rights reserved.
-//
-
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
     }
-
-
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
-
